5bf7215b55fad44045ed9d31d6683c7f58e0542c
Mohammad Afzal <afzalex.store@gmail.com>
2025-12-13 14:53:36 +0530

    Removed ubuntu, microservice-container, and cadence

--- ubuntu/config/install-nvm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Source https://tecadmin.net/how-to-install-nvm-on-ubuntu-20-04/
-touch ~/.bashrc
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
-
-source ~/.bashrc
-
-nvm install 14
